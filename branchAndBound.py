b355445d4542013554605dee97754eebeafe83db
Luiz paulo de souza fontes junior <82241012+LPfontes@users.noreply.github.com>
2024-04-25 20:01:30 -0300

    Função para criar o modelo do problema com a bliblioteca mip

--- branchAndBound.py
+++ branchAndBound.py
@@ -1,3 +1,4 @@
+from mip import *
 class Problema_linear:
     def __init__(self, quantidade_variaveis,quantidade_restricoes,coeficientes_objetivo,lista_restricoes):
         self.quantidade_variaveis = quantidade_variaveis
@@ -11,7 +12,6 @@ def ler_arquivo(nome_arquivo):
             linhas = arquivo.readlines()
             tamanho_da_lista = len(linhas)
             lista_restricoes = []
-            # Salvando cada linha em uma variável
             for i in range(tamanho_da_lista):
                 linha = linhas[i].split()
                 match i:
@@ -34,8 +34,21 @@ def ler_arquivo(nome_arquivo):
     except FileNotFoundError:
         print(f'O arquivo "{nome_arquivo}" não foi encontrado.')
 
-# Exemplo de uso
-nome_arquivo = 'teste1.txt'  # Substitua 'exemplo.txt' pelo nome do arquivo que deseja ler
+def Criar_modelo(problema):
+    model = Model(sense=MAXIMIZE, solver_name=CBC)
+    x = {i: model.add_var(var_type=CONTINUOUS, name=f'x_{i}', lb=0.0) for i in  range(1,problema.quantidade_variaveis+1)}
+    model.objective = xsum(problema.coeficientes_objetivo[i-1]*x[i] for i in range(1,problema.quantidade_variaveis+1))
+    for i in range(problema.quantidade_restricoes):
+        tamanho_lista = len(problema.lista_restricoes[i])
+        soma =  xsum(problema.lista_restricoes[i][j]*x[j+1] for j in range (tamanho_lista-2))
+        model +=soma <= problema.lista_restricoes[i][tamanho_lista-1]
+    model.write("model.lp") # salva modelo em arquivo
+    with open("model.lp") as f: # lê e exibe conteúdo do arquivo
+        print(f.read())
+    return model
+
+nome_arquivo = 'teste2.txt'  
 problema = ler_arquivo(nome_arquivo)
+model = Criar_modelo(problema)
 
     
