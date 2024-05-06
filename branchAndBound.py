f5561cacf4b97d8a838dd84cb7a74c978b58895e
Luiz paulo de souza fontes junior <82241012+LPfontes@users.noreply.github.com>
2024-05-05 23:32:36 -0300

    ultimas alterações

--- branchAndBound.py
+++ branchAndBound.py
@@ -1,6 +1,6 @@
 from mip import *
 from modulo_Problema_linear import Problema_linear
-
+import copy
 def ler_arquivo(nome_arquivo):
     try:
         with open(nome_arquivo, 'r') as arquivo:
@@ -29,16 +29,86 @@ def ler_arquivo(nome_arquivo):
     except FileNotFoundError:
         print(f'O arquivo "{nome_arquivo}" não foi encontrado.')
 
+def todas_variaveis_inteiras(modelo):
+        for var in modelo.modelo.vars:
+            if abs(var.x - round(var.x)) >= 1e-6:
+                return False
+        return True   
+    
+def who_is_closer(modelo):
+    var_prox_05 = None
+    melhor_dist = 1000
 
-nome_arquivo = 'teste2.txt'  
-quantidade_variaveis,quantidade_restricoes,coeficientes_objetivo,lista_restricoes = ler_arquivo(nome_arquivo)
-tipo_das_variaveis = BINARY
-problema = Problema_linear(nome_arquivo,tipo_das_variaveis,quantidade_variaveis,quantidade_restricoes,coeficientes_objetivo,lista_restricoes) 
-problema.resolver()
+    for v in modelo.modelo.vars:
+        dist = abs(v.x - 0.5)
 
+        if dist < melhor_dist:
+            var_prox_05 = v
+            melhor_dist = dist
+ 
+    return var_prox_05
 
+def branch_and_bound(problema):
 
+    # Definimos um valor para a inicialização
+    melhor_solucao = -float("inf")
+    # Variável que guarda o melhor modelo encontrado
+    melhor_problema = None
+    # Inicializando uma fila com os modelos abertos
+    fila = [problema]
 
+    # Só para quando a fila estiver vazia
+    while fila:
+        # Modelo atual é sempre o primeiro elemento da fila
+        problema_atual = fila[0]
+
+        status = problema_atual.resolver()
+
+        # Verifica se houve uma solução viável
+        if status == OptimizationStatus.OPTIMAL:
+
+            # Busca a variável mais próxima de 0.5
+            var_prox_05 = who_is_closer(problema_atual)
+            # Verifica se todas as variáveis são inteiras
+            inteiro = todas_variaveis_inteiras(problema_atual)
+            
+            if inteiro:
+                # Sendo todas inteiras, vemos se ela é a melhor solução atual
+                if problema_atual.modelo.objective_value > melhor_solucao:
+                    melhor_solucao = problema_atual.modelo.objective_value
+                    melhor_problema = problema_atual
+        
+            else:
+                # Não sendo inteira, criamos novas condições para solucionar o problema
+                if problema_atual.modelo.objective_value > melhor_solucao:
+        
+                    model1 = Problema_linear(problema_atual.modelo.copy())
+                    model1.adicionar_restricao(var_prox_05,1,">=","")
+                    model2 = Problema_linear(problema_atual.modelo.copy())
+                    model2.adicionar_restricao(var_prox_05,0,"<=","")
+
+                    # Adicionamos os novos modelos a lista 
+                    fila.append(model1)
+                    fila.append(model2)
+
+        # Removemos o modelo corrente da fila
+        fila.pop(0)
+    
+    return  melhor_problema 
+with open("saida.txt", "w") as arquivo:
+    for i in range(1, 5):
+
+        quantidade_variaveis,quantidade_restricoes,coeficientes_objetivo,lista_restricoes = ler_arquivo(f"teste{i}.txt")
+        problema = Problema_linear(None)
+        problema.criarModelo(f"teste{i}.txt",CONTINUOUS,quantidade_variaveis,0.0,1.0,quantidade_restricoes,coeficientes_objetivo,lista_restricoes)
+
+        solucao = branch_and_bound(problema)
+   
+        print(f"Solution Teste {i}  = {solucao.modelo.objective_value:.2f}", file=arquivo)
+        arquivo.write("Solution:\n")
+        for v in solucao.modelo.vars:
+            print(f"{v.name} = {v.x:.2f}", file=arquivo)
+        print("--------------------------------------------------", file=arquivo)
 
 
 
--- modulo_Problema_linear.py
+++ modulo_Problema_linear.py
@@ -1,46 +1,49 @@
 from mip import *
 class Problema_linear:
-    def __init__(self,nome_arquivo,tipo_das_variaveis,quantidade_variaveis,quantidade_restricoes,coeficientes_objetivo,lista_restricoes):
-        self.model = Model(nome_arquivo,sense=MAXIMIZE, solver_name=CBC)
-        x = {i: self.model.add_var(var_type=tipo_das_variaveis, name=f'x_{i}', lb=0.0) for i in  range(1,quantidade_variaveis+1)}
-        self.model.objective = xsum(coeficientes_objetivo[i-1]*x[i] for i in range(1,quantidade_variaveis+1))
+    def __init__(self,modelo):
+        self.modelo = modelo
+    def criarModelo(self,nome_arquivo,tipo_das_variaveis,quantidade_variaveis,lower_bound,upper_bound,quantidade_restricoes,coeficientes_objetivo,lista_restricoes):
+        # Criação de um modelo de Maximização
+        self.modelo = Model(nome_arquivo,sense=MAXIMIZE, solver_name=CBC)
+        x = {i: self.modelo.add_var(var_type=tipo_das_variaveis, name=f'x_{i}', lb=lower_bound, ub=upper_bound) for i in  range(1,quantidade_variaveis+1)}
+        
+        self.modelo.objective = xsum(coeficientes_objetivo[i-1]*x[i] for i in range(1,quantidade_variaveis+1))
+
         for i in range(quantidade_restricoes):
             tamanho_lista = len(lista_restricoes[i])
-            soma =  xsum(lista_restricoes[i][j]*x[j+1] for j in range (tamanho_lista-2))
-            self.model +=soma <= lista_restricoes[i][tamanho_lista-1],"cost"+ str(i)
-        print("Modelo criado")
+            soma =  xsum(lista_restricoes[i][j]*x[j+1] for j in range (tamanho_lista-1))
+            self.modelo += soma <= lista_restricoes[i][tamanho_lista-1]
         self.salvar_modelo()
-        self.exibir_modelo()
+        
 
     def salvar_modelo(self):  
-        self.model.write("model.lp") 
+        self.modelo.write("model.lp") 
 
     def exibir_modelo(self):  
         with open("model.lp") as f: # lê e exibe conteúdo do arquivo
             print(f.read())   
 
     def remover_restricao(self,restriçao):
-        self.model.remove(restriçao)
+        self.modelo.remove(restriçao)
 
     def adicionar_restricao(self, variavel, valor_restricao, tipo, nome):
         match tipo:
             case "<=":
-                self.model.add_constr(variavel <= valor_restricao, nome)
+                self.modelo.add_constr(variavel <= valor_restricao, nome)
             case ">=":
-                self.model.add_constr(variavel >= valor_restricao, nome)
+                self.modelo.add_constr(variavel >= valor_restricao, nome)
             case "==":
-                self.model.add_constr(variavel == valor_restricao, nome)
+                self.modelo.add_constr(variavel == valor_restricao, nome)
             case _:
                 raise ValueError("Tipo de restrição inválido. Use '<=', '>=', ou '=='.")
             
     def resolver(self):
-        status = self.model.optimize(relax=True)
-        print("Status = ",  status)
-        print(f"Solution value  = {self.model.objective_value:.2f}\n")
-        print("Solution:")
-        for vars in self.model.vars:
-            print(f"{vars.name} = {vars.x:.2f}")
-            if not vars.x.is_integer():
-                self.variaveis_not_integer.append(vars) 
-        for vars in self.variaveis_not_integer:
-            print(vars.x)
+        self.variaveis_not_integer = []
+        status = self.modelo.optimize()
+        if status == OptimizationStatus.OPTIMAL:
+            for vars in self.modelo.vars:
+                if not vars.x.is_integer():
+                    self.variaveis_not_integer.append(vars) 
+            
+        return status
+    
